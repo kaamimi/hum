1f0f7b2d64780bb80506df64f70b812bf363a847
Kamalesh Khanna <141732572+kaamimi@users.noreply.github.com>
2025-08-04 19:10:48 +0530

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 
 import './core/theme.dart';
 import './views/timeline/timeline_view.dart';
-import './views/test_view.dart';
 
 class HumApp extends StatelessWidget {
   const HumApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: darkTheme, home: TestView());
+    return MaterialApp(theme: darkTheme, home: TimelineView());
   }
 }
